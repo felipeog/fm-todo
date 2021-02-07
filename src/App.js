cbe5098bc88f933e11a99248c81c45c6941cdf25
felipeog <felipeog476@gmail.com>
2021-02-07 18:33:35 -0300

    AppHeader init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import { h } from 'preact'
 
+import AppHeader from './components/AppHeader'
 import * as style from './App.module.scss'
 
 const App = () => {
-  return <div className={style.App}></div>
+  return (
+    <main className={`App ${style.root}`}>
+      <AppHeader />
+    </main>
+  )
 }
 
 export default App
---
+++ src/components/AppHeader/index.js
@@ -0,0 +1,38 @@
+import { h } from 'preact'
+import { useState } from 'preact/hooks'
+
+import Sun from 'url:../../assets/img/icon-sun.svg'
+import Moon from 'url:../../assets/img/icon-moon.svg'
+import * as style from './index.module.scss'
+
+const themes = {
+  dark: 'dark',
+  light: 'light',
+}
+
+const AppHeader = () => {
+  const [theme, setTheme] = useState(themes.dark)
+
+  const toggleTheme = () => {
+    setTheme((currentTheme) => {
+      const nextTheme =
+        currentTheme === themes.dark ? themes.light : themes.dark
+
+      document.querySelector('html').setAttribute('data-theme', nextTheme)
+
+      return nextTheme
+    })
+  }
+
+  return (
+    <header className={`AppHeader ${style.root}`}>
+      <h1 className={style.title}>Todo</h1>
+
+      <button className={style.button} onClick={toggleTheme}>
+        {theme === themes.dark ? <img src={Sun} /> : <img src={Moon} />}
+      </button>
+    </header>
+  )
+}
+
+export default AppHeader
