e1b7257411f336abec2943ca8f6026765971a037
felipeog <felipeog476@gmail.com>
2021-02-08 14:25:33 -0300

    Add TodoInput

---
+++ src/components/TodoInput/index.js
@@ -0,0 +1,46 @@
+import React, { useState, useCallback } from 'react'
+
+import Checkbox from '../Checkbox'
+import * as style from './index.module.scss'
+
+const TodoInput = ({ setTodos }) => {
+  const [isChecked, setIsChecked] = useState(false)
+  const [todoTitle, setTodoTitle] = useState('')
+
+  const toggleIsChecked = () => setIsChecked((isChecked) => !isChecked)
+
+  const handleTitleChange = (event) => setTodoTitle(event.target.value)
+
+  const addTodo = useCallback(
+    (event) => {
+      event.preventDefault()
+
+      setTodos((todos) => [
+        ...todos,
+        {
+          title: todoTitle,
+          checked: isChecked,
+          id: `draggable-${Math.round(Math.random() * 999_999)}`,
+        },
+      ])
+      setIsChecked(false)
+      setTodoTitle('')
+    },
+    [isChecked, todoTitle],
+  )
+
+  return (
+    <form className={`TodoInput ${style.root}`} onSubmit={addTodo}>
+      <Checkbox checked={isChecked} onClick={toggleIsChecked} />
+      <input
+        className={style.input}
+        type="text"
+        value={todoTitle}
+        onChange={handleTitleChange}
+        placeholder="Create a new todo..."
+      />
+    </form>
+  )
+}
+
+export default TodoInput
