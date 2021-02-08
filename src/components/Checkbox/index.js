03bfdd489afa2c172032930b8d4a487fae301946
felipeog <felipeog476@gmail.com>
2021-02-08 14:44:23 -0300

    Fix Checkbox state

--- src/components/Checkbox/index.js
+++ src/components/Checkbox/index.js
@@ -2,13 +2,15 @@ import React from 'react'
 
 import * as style from './index.module.scss'
 
-const Checkbox = ({ isChecked, ...props }) => (
-  <input
-    className={`Checkbox ${style.root}`}
-    type="checkbox"
-    checked={isChecked}
-    {...props}
-  />
-)
+const Checkbox = ({ isChecked, ...props }) => {
+  console.log({ style, isChecked })
+  return (
+    <input
+      className={`Checkbox ${style.root} ${isChecked ? style.checked : ''}`}
+      type="checkbox"
+      {...props}
+    />
+  )
+}
 
 export default Checkbox
